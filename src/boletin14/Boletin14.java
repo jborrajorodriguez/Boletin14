e65568b9608cdedd2e36617a0e54f7fd57c99a8c
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-12-11 09:48:59 +0100

    Inicio proyecto

---
+++ src/boletin14/Boletin14.java
@@ -0,0 +1,17 @@
+package boletin14;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Boletin14 {
+
+    public static void main(String[] args) {
+        
+       Garaxe c1=new Garaxe();
+       c1.setMatricula("321X");
+//       c1.entradaGaraxe();
+        System.out.println(c1.toString());
+       
+
+    }
+
+}
