d20aa9b5a4dccfdd605529097d119450a974fd1f
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-12-12 09:16:19 +0100

    Clase main

--- src/boletin14/Boletin14.java
+++ src/boletin14/Boletin14.java
@@ -6,10 +6,23 @@ public class Boletin14 {
 
     public static void main(String[] args) {
         
-       Garaxe c1=new Garaxe();
-       c1.setMatricula("321X");
-//       c1.entradaGaraxe();
-        System.out.println(c1.toString());
+      Coches c1=new Coches("456X");
+      Coches c2=new Coches("482P");
+      Coches c3=new Coches("213R");
+      Coches c4=new Coches("123Y");
+      Coches c5=new Coches("12DP");
+      
+      Garaxe gar=new Garaxe();
+      gar.entradaGaraxe();
+      gar.entradaGaraxe();
+      gar.entradaGaraxe();
+      gar.entradaGaraxe();
+      gar.entradaGaraxe();
+      
+      
+      
+      
+     
        
 
     }
